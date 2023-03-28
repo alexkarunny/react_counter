27cfc2324f3446f9001f2070c7f8084cf5ba504e
alexkarunny <smetan4ik1@gmail.com>
2023-03-28 23:48:10 +0300

    02_refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,68 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 
 function App() {
-    let [value, setValue] = useState<number>(0)
+    let [value, setValue] = useState<number | string>(0)
+
+    let [minValue, setMinValue] = useState<number>(0)
+
+    let [maxValue, setMaxValue] = useState<number>(5)
+
+    let [isButtonSetDisabled, setIsButtonSetDisabled] = useState<boolean>(true)
+
+    let [isButtonIncDisabled, setIsButtonIncDisabled] = useState<boolean>(false)
+
 
     const onClickIncreaseHandler = () => {
-        setValue(++value)
+
+
     }
 
     const onClickResetValueHandler = () => {
-        setValue(0)
+        setValue(minValue)
     }
+
+    const onChangeSetMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setMaxValue(+e.currentTarget.value)
+        setValue('set numbers')
+        setIsButtonIncDisabled(true)
+        setIsButtonSetDisabled(false)
+    }
+
+    const onChangeSetMinValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setMinValue(+e.currentTarget.value)
+        setValue('set numbers')
+        setIsButtonIncDisabled(true)
+        setIsButtonSetDisabled(false)
+    }
+
+    const onClickSetValueHandler = () => {
+        setValue(minValue)
+        setIsButtonSetDisabled(true)
+        setIsButtonIncDisabled(false)
+    }
+
+    let finalButtonSetIsDisabled = isButtonSetDisabled || minValue === maxValue || minValue < 0 || minValue > maxValue
 
     return (
         <div className="App">
-            <div className={value === 5 ? 'red' : ''}>{value}</div>
-            <button onClick={onClickIncreaseHandler} disabled={value === 5}>+</button>
-            <button onClick={onClickResetValueHandler}>reset</button>
+            <div className={value === maxValue ? 'red' : ''}>{value}</div>
+            <button onClick={onClickIncreaseHandler} disabled={isButtonIncDisabled}>+</button>
+            <button onClick={onClickResetValueHandler} disabled={isButtonIncDisabled}>reset</button>
+            <div>
+                <input type="number" onChange={onChangeSetMinValueHandler} value={minValue}/>
+                <span>MinValue</span>
+            </div>
+            <div>
+                <input type="number" onChange={onChangeSetMaxValueHandler} value={maxValue}/>
+                <span>MaxValue</span>
+            </div>
+
+            <div>
+                <button onClick={onClickSetValueHandler} disabled={finalButtonSetIsDisabled}>Set</button>
+            </div>
+
+
         </div>
     );
 }
